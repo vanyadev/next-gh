c33ff35e698fe405b6cbef7802f5714a3b8409e9
vanyadev <ivan.dev.hrytsenko@gmail.com>
2023-09-20 16:37:59 +0300

    moving env to nex.config

--- app/helpers/fetchLanguages.ts
+++ app/helpers/fetchLanguages.ts
@@ -2,10 +2,7 @@ export async function fetchLanguages(languages_url: string) {
   const response = await fetch(languages_url, {
     method: "GET",
     headers: {
-      Authorization: `Bearer ${
-        process.env.GITHUB_KEY ||
-        "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"
-      }`,
+      Authorization: `Bearer ${process.env.GITHUB_KEY}`,
       "User-Agent": "my-app",
     },
   });
--- next.config.js
+++ next.config.js
@@ -1,8 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ["avatars.githubusercontent.com"],
-      },
-}
+  images: {
+    domains: ["avatars.githubusercontent.com"],
+  },
+  env: {
+    GITHUB_KEY:
+      "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",
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
